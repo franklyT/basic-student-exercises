7f304b809d7f8112f668f7c254a6cc7c5dfcfd8f
franklyT <48483484+franklyT@users.noreply.github.com>
2021-01-01 22:59:52 -0500

    Update hangman_class_based.py

--- Python/hangman/hangman_class_based.py
+++ Python/hangman/hangman_class_based.py
@@ -1,4 +1,6 @@
 from random import choice
+from typing import TextIO
+
 
 class Game:
     def __init__(self):
@@ -6,7 +8,7 @@ class Game:
         self.choose_word()
         self.thread()
 
-    images = [
+    images: list = [
         '''
            +---+
                |
@@ -38,48 +40,54 @@ class Game:
         '''
            +---+
            O   |
-          /|\  |
+          /|\\  |
                |
              =====
         ''',
         '''
            +---+
            O   |
-          /|\  |
+          /|\\  |
           /    |
              =====
         ''',
         '''
            +---+
            O   |
-          /|\  |
-          / \  |
+          /|\\  |
+          / \\  |
              =====
         ''']
 
-    word_list = []
-    chosen_word = ""
-    guesses = ""
-    guess_increment = 0
-    chances = 0
+    word_list: list = []
+    chosen_word: str = ""
+    guesses: str = ""
+    guess: str = ""
+    guess_increment: int = 0
+    chances: int = 0
 
     def build_dictionary(self):
-        open_file = open('../words.txt')
+        open_file: TextIO = open('./words.txt')
 
         for line in open_file:
             line = line.strip().lower()
 
-            if len(line) < 4 or len(line) > 7: continue
-            if not line.isalpha(): continue
+            if self.is_line_clean(line):
+                self.word_list.append(line)
 
-            self.word_list.append(line)
+    @staticmethod
+    def is_line_clean(line: str):
+        if ((len(line) < 4 or len(line)) > 8) or not line.isalpha():
+            return False
+        else:
+            return True
 
     def choose_word(self):
         self.chosen_word = choice(self.word_list)
 
     def thread(self):
         while self.chances < len(self.images):
-            self.guess()
+            self.player_guess()
             self.word_feedback()
             print(self.images[self.chances])
 
@@ -87,35 +95,51 @@ class Game:
         self.guess_increment = 0
 
         for char in self.chosen_word:
-            if char in self.guesses: 
-                print(char, end = " ")
+            if char in self.guesses:
+                print(char, end=" ")
                 self.guess_increment += 1
-            else: 
-                print("_", end = " ")
-            
+            else:
+                print("_", end=" ")
+
             if self.guess_increment == len(self.chosen_word):
                 print("\nYou win!")
-                raise 'Game over!'
-            
+                print("The word was " + self.chosen_word + "!")
+                self.game_over(True)
+
         print("\n\nLetters guessed: " + self.guesses)
 
-    def guess(self):
-        guess = input("Guess a letter: ")
+    @staticmethod
+    def game_over(state: bool):
+        if state:
+            raise Exception("You win!")
+        else:
+            raise Exception("You lose!")
 
-        while not (guess.isalpha()) or len(guess) > 1:
-            guess = input("Invalid input. Please guess a letter: ")
-        
-        self.guesses += guess
+    def player_guess(self):
+        self.guess = input("Guess a letter: ")
 
-        if guess in self.chosen_word:
+        self.validate_guess()
+
+        self.guesses += self.guess
+
+        self.check_guess()
+
+    def validate_guess(self):
+        while (not self.guess.isalpha()) or len(self.guess) > 1 or self.guess in self.guesses:
+            self.guess = input("Invalid input. Please guess a letter: ")
+
+    def check_guess(self):
+        if self.guess in self.chosen_word:
             print("You guessed right!")
         else:
             print("You guessed wrong!")
             self.chances += 1
+
             if self.chances == len(self.images) - 1:
                 print("You lose!")
+                print("The word was " + self.chosen_word + "!")
                 print(self.images[len(self.images) - 1])
-                raise "You lose!"
-    
+                self.game_over(False)
 
-hangman = Game()
+
+Game()
