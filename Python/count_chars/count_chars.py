9303f8f4886daed2480f851753d2cdc6b0a64ea8
franklyT <48483484+franklyT@users.noreply.github.com>
2021-01-20 01:23:49 -0500

    Update count_chars.py

--- Python/count_chars/count_chars.py
+++ Python/count_chars/count_chars.py
@@ -1,12 +1,12 @@
-def count_char(str, char):
+def count_chars_lambda(str, char):
     return len(list(filter(lambda char2: char2 == char, str)))
 
 # Simple version
-def simple_count_chars(str, char):
+def count_chars(str, char):
     count = 0
     for i in str: 
         if i == char: 
             count = count + 1
     return count
 
-print(count_char('abbc', 'b'))
+print(count_chars('abbc', 'b'))
