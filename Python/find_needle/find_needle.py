3f3d0821c69a6023cc5ae6e3b4c5538c2381f307
franklyT <48483484+franklyT@users.noreply.github.com>
2021-01-20 01:51:34 -0500

    Update find_needle.py

--- Python/find_needle/find_needle.py
+++ Python/find_needle/find_needle.py
@@ -1,7 +1,6 @@
 def find_needle(haystack):
     for i, x in enumerate(haystack):
         if x == 'needle': 
-            print('found the needle at position ' + str(i))
+            return 'found the needle at position ' + str(i)
             return
-    print(None)
-    return
+    return None
