fb6495c35b5b8fd65437e23186eb6dee919ff09d
franklyT <48483484+franklyT@users.noreply.github.com>
2021-01-20 01:21:51 -0500

    Update and rename count_Bs_count_char.py to count_chars.py

--- Python/count_chars/count_chars.py
+++ Python/count_chars/count_chars.py
@@ -1,17 +1,12 @@
-def count_Bs(str):
-    return len(list(filter(lambda char: char == "B", str)))
-
 def count_char(str, char):
     return len(list(filter(lambda char2: char2 == char, str)))
 
-# Simple version for students
-def simple_count_Bs(str):
+# Simple version
+def simple_count_chars(str, char):
     count = 0
     for i in str: 
-        if i == 'B': 
+        if i == char: 
             count = count + 1
     return count
-
-print(count_Bs('BBBbbb'))
 
 print(count_char('abbc', 'b'))
