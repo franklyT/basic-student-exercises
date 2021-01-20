622533f1e144722a224b50fcaa4f4bdd8f8f4893
franklyT <48483484+franklyT@users.noreply.github.com>
2021-01-20 01:36:01 -0500

    Update checkerboard.py

--- Python/checkerboard/checkerboard.py
+++ Python/checkerboard/checkerboard.py
@@ -1,13 +1,12 @@
-rows = 8
-cols = 8
+# Your code goes under this line
 
-for i in range(0, rows):
-    checkerboard = ""
-    
-    for j in range(0, cols):
-        if (j + i) % 2 != 0:
-            checkerboard += "#"
-        else:
-            checkerboard += " "
-    
-    print(checkerboard)
+def checkerboard(rows, cols):
+    the_board = ""
+    for i in range(0, rows):
+    	for j in range(0, cols):
+        	if (j + i) % 2 != 0:
+            	the_board += "#"
+        	else:
+            	the_board += " "
+            the_board += "\n"
+    return the_board
