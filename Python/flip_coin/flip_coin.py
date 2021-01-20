b061999be480a77dbc52755b42770fb9d0dc24dc
franklyT <48483484+franklyT@users.noreply.github.com>
2021-01-20 02:06:47 -0500

    Update flip_coin.py

--- Python/flip_coin/flip_coin.py
+++ Python/flip_coin/flip_coin.py
@@ -13,15 +13,4 @@ def flip_coin(flips):
     heads_probability = (round(heads_count/flips, 1) * 100)
     tails_probability = (round(tails_count/flips, 1) * 100)
 
-    print("Heads")
-    print("Count: " + str(heads_count))
-    print("Probability: ~" + str(heads_probability) + "%")
-    
-    print("")
-    
-    print("Tails")
-    print("Count: " + str(tails_count))
-    print("Probability: ~" + str(tails_probability) + "%")
-    
-    
-flip_coin(100000)
+    return "Heads Probability: ~" + str(heads_probability) + "%" + " Tails Probability: ~" + str(tails_probability) + "%" 
