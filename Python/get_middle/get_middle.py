7f9eaafb867fa15d27d48cec1ab0febaa0c889bc
franklyT <48483484+franklyT@users.noreply.github.com>
2021-01-20 01:50:39 -0500

    Update get_middle.py

--- Python/get_middle/get_middle.py
+++ Python/get_middle/get_middle.py
@@ -1,7 +1,5 @@
 import math
 
-def getMiddle(str):
+def get_middle(str):
     if len(str) % 2 == 0: return str[int(len(str) / 2 - 1)] + str[int(len(str) / 2)];
     return str[math.floor(len(str) / 2)]
-
-print(getMiddle("Helllo"))
